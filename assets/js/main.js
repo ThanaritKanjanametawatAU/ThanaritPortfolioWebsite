d05457a633dd92b9db2103bfc7f759f381ef86aa
Thanarit <u6410322@au.edu>
2024-06-30 02:42:36 +0700

    chore: Update hue value for first-color in styles.css and add scroll reveal animation in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,3 +104,17 @@ window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+});
+
+sr.reveal(`.home__data, .experience, .skills, .contact__container`)
+sr.reveal(`.home__img`, {delay: 100})
+sr.reveal(`.home__scroll`, {delay: 200})
+sr.reveal(`.work__card, .service__card`, {interval: 100})
+sr.reveal(`.about__content`, {origin: 'right'}) 
+sr.reveal(`.about__img`, {origin: 'left'})
